232f1a4f25db422b53f1cf9659eafb67deec10e6
AlexeyWu <saint1418@gmail.com>
2023-03-19 23:37:20 +0300

    correct files sh

--- 04zfs/test.sh
+++ 04zfs/test.sh
@@ -5,7 +5,6 @@ echo sudo -i
 yum install -y http://download.zfsonlinux.org/epel/zfs-release.el7_8.noarch.rpm
 #import gpg key 
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
-#yum update -y
 #install DKMS style packages for correct work ZFS
 yum install -y epel-release kernel-devel zfs
 #change ZFS repo
@@ -13,11 +12,6 @@ yum-config-manager --disable zfs
 yum-config-manager --enable zfs-kmod
 yum -y install zfs
 #Add kernel module zfs
-echo sudo modprobe zfs
-#for directory in /lib/modules/*; do
-#  kernel_version=$(basename $directory)
-#  dkms autoinstall -k $kernel_version
-#done
 echo sudo modprobe zfs
 #install wget
 yum install -y wget
